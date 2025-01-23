85f3d78d36d36be3b7814be97f4da72e3c4dce1b
Mathieu <mathieusoufflard@gmail.com>
2025-01-23 17:49:07 +0100

    add UI of title + city list
    it's just UI with hard value

--- app/lib/ui/city_list.dart
+++ app/lib/ui/city_list.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CityList extends StatefulWidget{
@@ -12,13 +11,77 @@ class _CityListState extends State<CityList>{
   @override
   Widget build(BuildContext context) {
 
-  return Scaffold(
-      body: Container(
-        child: const Center(
-          child: Text('Home Page'),
-        ),
-  )
-      );
+  return Stack(
+    children: [
+      title(),
+      list(),
+    ],
+  );
+
 
   }
+
+  title() => const Align(
+    alignment: Alignment.topCenter,child: Text(
+    'NeoWeather',
+    style: TextStyle(
+      color: Colors.white,
+      fontSize: 36,
+      fontWeight: FontWeight.bold,
+    ),
+  ),
+  );
+
+  list() => Center(
+    child: Container(
+      constraints: const BoxConstraints(maxHeight: 600.0),
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      child: ListView.builder(
+        padding: const EdgeInsets.only(left: 5, right: 5),
+        shrinkWrap: true,
+        itemCount: 15,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: (){
+              print('tap');
+            },
+            child:Container(
+              height: 90,
+              margin: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+                color: Colors.white,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Image.asset('assets/weather_icon/broken_clouds.png', scale: 1.5, color: Colors.black,),
+                      const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Paris',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text('Nuageux'),
+                        ],
+                      ),
+                      const Text('8',
+                        style:  TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ]
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    ),
+  );
 }
