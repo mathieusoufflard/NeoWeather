452332f68b68a96eb7cc50227f1a5ea5d1823147
Mathieu <mathieusoufflard@gmail.com>
2025-01-27 16:38:15 +0100

    change UI + add some UI element

--- app/lib/ui/weather_details.dart
+++ app/lib/ui/weather_details.dart
@@ -1,66 +1,133 @@
 import 'package:app/ui/widget/gradient_scaffold.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class WeatherDetails extends StatelessWidget{
+class WeatherDetails extends StatelessWidget {
   const WeatherDetails({super.key});
 
   @override
   Widget build(BuildContext context) {
     return GradientScaffold(
-      child: Stack(
+      child: Column(
         children: [
-          Row(
-            children: [
-              backButton(context),
-              title(),
-            ],
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                backButton(context),
+                Expanded(
+                  child: Center(
+                    child: title(),
+                  ),
+                ),
+              ],
+            ),
           ),
-          mainData(),
-          weatherDetailsCard(),
+
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 40),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  mainData(),
+                  weatherDetailsCard(),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 
-  backButton(BuildContext context) => Align(
-    alignment: Alignment.topLeft,
-    child: Padding(
-      padding: const EdgeInsets.only(left: 10),
-      child: Align(
-          alignment: Alignment.topLeft,
-          child: GestureDetector(
-            onTap: (){
-              Navigator.pop(context);
-            },
-            child: Image.asset(
-              'assets/navigation/back_arrow.png',
-              color: Colors.black,
-              scale: 1.75,
-            ),
-          )
-      ),
+  Widget backButton(BuildContext context) => GestureDetector(
+    onTap: () {
+      Navigator.pop(context);
+    },
+    child: Image.asset(
+      'assets/navigation/back_arrow.png',
+      color: Colors.black,
+      scale: 1.75,
     ),
   );
 
-  title() => Expanded(
-    child: Padding(
-      padding: EdgeInsets.only(right: 20, left: 10),
-      child: Align(
-        alignment: Alignment.topCenter,
-        child: const Align(
-          alignment: Alignment.topCenter,
-          child: Text(
-            'Paris',
+  Widget title() => const Text(
+    'Paris',
+    style: TextStyle(
+      color: Colors.white,
+      fontSize: 36,
+      fontWeight: FontWeight.bold,
+    ),
+    maxLines: 1,
+    overflow: TextOverflow.ellipsis,
+  );
+
+  Widget mainData() => Column(
+    children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+              padding: EdgeInsets.only(right: 10),
+            child: Image.asset(
+              'assets/weather_icon/clear_sky.png',
+              scale: 1.5,
+            ),
+          ),
+          Text(
+              '8°',
             style: TextStyle(
               color: Colors.white,
-              fontSize: 36,
+              fontSize: 32,
               fontWeight: FontWeight.bold,
             ),
           ),
+        ],
+      ),
+      Text(
+        'Nuageux',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
         ),
       ),
-    ),
+      Text(
+        'Resentie 6°',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+              padding: EdgeInsets.only(right: 10),
+            child: Text(
+              'min: 1',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 10),
+            child: Text(
+              'max: 10',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ],
+      )
+    ],
   );
 
   mainData() => Container();
