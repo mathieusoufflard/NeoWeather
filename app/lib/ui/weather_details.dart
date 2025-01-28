c5e90d0fdc5aa3f55475bcd62e2d4f8251ef35eb
Mathieu <mathieusoufflard@gmail.com>
2025-01-28 15:40:17 +0100

    upadate all card
    - use custom card ans text
    - add card weatherByHourCard(), weatherByDayCard(), weatherSunsetCard(), weatherHumidityCard(), weatherVisibilityCard(), weatherWindSpeedCard(), weatherUVCard(), weatherPressureCard()
    - all those cards are placed in row in column
    - all those cards have juste a title

--- app/lib/ui/weather_details.dart
+++ app/lib/ui/weather_details.dart
@@ -28,13 +28,32 @@ class WeatherDetails extends StatelessWidget {
           Expanded(
             child: Padding(
               padding: const EdgeInsets.only(top: 40),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  mainData(),
-                  weatherAlertCard(),
-                ],
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    mainData(),
+                    weatherAlertCard(),
+                    weatherByHourCard(),
+                    weatherByDayCard(),
+                    weatherSunsetCard(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        weatherHumidityCard(),
+                        weatherVisibilityCard(),
+                        weatherWindSpeedCard(),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        weatherUVCard(),
+                        weatherPressureCard(),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
@@ -88,54 +107,185 @@ class WeatherDetails extends StatelessWidget {
   );
 
   weatherAlertCard() => AppWidgets.customCard(
-      color: const Color.fromRGBO(216, 0, 4, 0.25),
+    paddingLeft: 20,
+    paddingRight: 20,
+    color: const Color.fromRGBO(216, 0, 4, 0.25),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Align(
+          alignment: Alignment.centerLeft,
+          child: AppWidgets.customText(text: 'Alerte', color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 18,),
+        AppWidgets.customText(text: 'Meteo_France', color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+        SizedBox(height: 18,),
+        AppWidgets.customText(text: 'Avis aux petites embarcations', color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+        SizedBox(height: 18,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            AppWidgets.customText(text: 'Debut 10:00', color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+            AppWidgets.customText(text: 'Fin 15:00', color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+          ],
+        ),
+        SizedBox(height: 18,),
+        Container(
+          height: 14 * 10 + 20,
+          child: SingleChildScrollView(
+            child: AppWidgets.customText(text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,'
+                'molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum'
+                'numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium'
+                'optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis'
+                'obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam'
+                'nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,'
+                'tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,'
+                'quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos'
+                'sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam'
+                'recusandae alias error harum maxime adipisci amet laborum. Perspiciatis'
+                'minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit'
+                'quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur'
+                'fugiat, temporibus enim commodi iusto libero magni deleniti quod quam'
+                'consequuntur! Commodi minima excepturi repudiandae velit hic maxime'
+                'doloremque. Quaerat provident commodi consectetur veniam similique ad'
+                'earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo'
+                'fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore'
+                'suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium'
+                'modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam'
+                'totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam'
+                'quasi aliquam eligendi, placeat qui corporis!', color: Colors.white, fontSize: 14, height: 1.2),
+          ),
+        ),
+      ],
+    ),
+  );
+
+  weatherByHourCard() => AppWidgets.customCard(
+    paddingLeft: 20,
+    paddingRight: 20,
+      color: Color.fromRGBO(16, 64, 132, 0.25),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
         children: [
           Align(
             alignment: Alignment.centerLeft,
-            child: AppWidgets.customText(text: 'Alerte', color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+            child: AppWidgets.customText(text: 'Prévision par heure', color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
           ),
           SizedBox(height: 18,),
-          AppWidgets.customText(text: 'Meteo_France', color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-          SizedBox(height: 18,),
-          AppWidgets.customText(text: 'Avis aux petites embarcations', color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-          SizedBox(height: 18,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              AppWidgets.customText(text: 'Debut 10:00', color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-              AppWidgets.customText(text: 'Fin 15:00', color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-            ],
-          ),
-          SizedBox(height: 18,),
-          Container(
-            height: 14 * 10 + 20,
-            child: SingleChildScrollView(
-              child: AppWidgets.customText(text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,'
-              'molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum'
-                  'numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium'
-                  'optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis'
-                  'obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam'
-                  'nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,'
-                  'tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,'
-                  'quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos'
-                  'sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam'
-                  'recusandae alias error harum maxime adipisci amet laborum. Perspiciatis'
-                  'minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit'
-                  'quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur'
-                  'fugiat, temporibus enim commodi iusto libero magni deleniti quod quam'
-                  'consequuntur! Commodi minima excepturi repudiandae velit hic maxime'
-                  'doloremque. Quaerat provident commodi consectetur veniam similique ad'
-                  'earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo'
-                  'fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore'
-                  'suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium'
-                  'modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam'
-                  'totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam'
-                  'quasi aliquam eligendi, placeat qui corporis!', color: Colors.white, fontSize: 14, height: 1.2),
-            ),
-          ),
         ],
       ),
+  );
+
+  weatherByDayCard() => AppWidgets.customCard(
+    paddingLeft: 20,
+    paddingRight: 20,
+    color: Color.fromRGBO(16, 64, 132, 0.25),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Align(
+          alignment: Alignment.centerLeft,
+          child: AppWidgets.customText(text: 'Prévision par jours', color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 18,),
+      ],
+    ),
+  );
+
+  weatherSunsetCard() => AppWidgets.customCard(
+    paddingLeft: 20,
+    paddingRight: 20,
+    color: Color.fromRGBO(16, 64, 132, 0.25),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Align(
+          alignment: Alignment.centerLeft,
+          child: AppWidgets.customText(text: 'Lever et coucher du soleil', color: Colors.white, fontSize: 13, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 18,),
+      ],
+    ),
+  );
+
+  weatherHumidityCard() => AppWidgets.customCard(
+    paddingLeft: 20,
+    paddingRight: 10,
+    color: Color.fromRGBO(16, 64, 132, 0.25),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Align(
+          alignment: Alignment.centerLeft,
+          child: AppWidgets.customText(text: 'Humidité', color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 18,),
+      ],
+    ),
+  );
+
+  weatherVisibilityCard() => AppWidgets.customCard(
+    paddingLeft: 10,
+    paddingRight: 10,
+    color: Color.fromRGBO(16, 64, 132, 0.25),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Align(
+          alignment: Alignment.centerLeft,
+          child: AppWidgets.customText(text: 'Visibilité', color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 18,),
+      ],
+    ),
+  );
+
+  weatherWindSpeedCard() => AppWidgets.customCard(
+    paddingLeft: 10,
+    paddingRight: 20,
+    color: Color.fromRGBO(16, 64, 132, 0.25),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Align(
+          alignment: Alignment.centerLeft,
+          child: AppWidgets.customText(text: 'Vent', color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 18,),
+      ],
+    ),
+  );
+
+  weatherUVCard() => AppWidgets.customCard(
+    paddingLeft: 20,
+    paddingRight: 10,
+    color: Color.fromRGBO(16, 64, 132, 0.25),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Align(
+          alignment: Alignment.centerLeft,
+          child: AppWidgets.customText(text: 'Indice UV', color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 18,),
+      ],
+    ),
+  );
+
+  weatherPressureCard() => AppWidgets.customCard(
+    paddingLeft: 10,
+    paddingRight: 20,
+    color: Color.fromRGBO(16, 64, 132, 0.25),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Align(
+          alignment: Alignment.centerLeft,
+          child: AppWidgets.customText(text: 'Préssion', color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 18,),
+      ],
+    ),
   );
 }
