a806021a1d7519c913614524b40eb92bfa1510e1
Mathieu <mathieusoufflard@gmail.com>
2025-02-01 19:30:15 +0100

    add path_provider to init hive

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,10 +3,35 @@ import 'package:app/ui/widget_utils/gradient_scaffold.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'hive_data_base/city_adapter.dart';
+import 'hive_data_base/current_weather_adapter.dart';
+import 'hive_data_base/daily_temperature_adapter.dart';
+import 'hive_data_base/daily_weather_adapter.dart';
+import 'hive_data_base/hourly_weather_adapter.dart';
+import 'hive_data_base/weather_alert_adapter.dart';
+import 'hive_data_base/weather_condition_adapter.dart';
+import 'hive_data_base/weather_data_adapter.dart';
+import 'model/city.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 
 void main() async{
   await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
+  final appDocumentDir = await path_provider.getApplicationDocumentsDirectory();
+  Hive.init('${appDocumentDir.path}/hive_database');
+
+  Hive.registerAdapter(CityAdapter());
+  Hive.registerAdapter(WeatherDataAdapter());
+  Hive.registerAdapter(CurrentWeatherAdapter());
+  Hive.registerAdapter(WeatherConditionAdapter());
+  Hive.registerAdapter(HourlyWeatherAdapter());
+  Hive.registerAdapter(DailyWeatherAdapter());
+  Hive.registerAdapter(DailyTemperatureAdapter());
+  Hive.registerAdapter(WeatherAlertAdapter());
+
+  await Hive.openBox<City>('cities');
+
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((value) => runApp(const MyApp()));
 }
